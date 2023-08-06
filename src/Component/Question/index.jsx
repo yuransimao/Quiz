6050171809a6d416d13d68c62abae6c6aef337b5
Yuran Simao <yuransimao7@gmail.com>
2023-08-06 03:06:14 +0100

    update

--- src/Component/Question/index.jsx
+++ src/Component/Question/index.jsx
@@ -38,9 +38,13 @@ export const Questions = () => {
   const counterTime = () =>{
     setCounter( s => s - 1 )
   }
+ 
 
-  const RestCounter =() =>{
+  const ClickHandle = () =>{
+    dispatch({type: Types.NEXT_QUESTION}); 
+    
     setCounter(state.Countertime)
+
   }
   useEffect(() =>{
 
@@ -84,7 +88,7 @@ export const Questions = () => {
 
       ))}
       <div className={Styles.Btns}>
-        {state.AnswerSelects && <Button handleClick={() => {dispatch({type: Types.NEXT_QUESTION}); RestCounter()}}
+        {state.AnswerSelects && <Button handleClick={ClickHandle }
         text="Continuar"
         />}
       </div>
